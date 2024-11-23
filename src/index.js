4a81ad709a0afe62bf4ff46ca92825142bec04aa
Vitor Quadros <vitoralgusto472@gmail.com>
2024-11-23 13:32:03 -0300

    ajustado funcionalidade de portas para a aplicação na AWS

--- src/index.js
+++ src/index.js
@@ -3,8 +3,9 @@ const app = require("./app");
 const PORT = process.env.PORT || 3000;
 
 const start = () => {
-	app.listen({ port: PORT });
+	app.listen({ port: PORT, host: "0.0.0.0" });
 	app.log.info("Server is running on http://localhost:" + PORT);
 };
+
 
 start();
